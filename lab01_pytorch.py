ecfc8802a64adbea92f1837698ffbc50d3fb4777
shoaib <shoaibkhanz@hotmail.com>
2022-09-19 16:52:47 +0100

    added more packages and lab01 script

--- lab01_pytorch.py
+++ lab01_pytorch.py
@@ -3,6 +3,8 @@ import requests
 import torch
 import pickle
 import gzip
+import wandb
+import random
 
 
 def download_data(path):
@@ -11,7 +13,7 @@ def download_data(path):
     :param path: 
 
     """
-    url = "https://github.com/pytorch/tutorials/raw/master/static/"
+    url = "https://github.com/pytorch/tutorials/raw/master/_static/"
     filename = "mnist.pkl.gz"
 
     if not (path / filename).exists():
@@ -20,11 +22,34 @@ def download_data(path):
 
     return path / filename
 
-# if __name__ == "__main__":
-#
-#     data_path = Path("data") if Path("data").exists() else Path("../data")
-#     path = data_path / "downloaded" / "vector-mnist"
-#     path.mkdir(parents=True, exist_ok=True)
-#
-#     datafile = download_data(path)
-#     print(datafile)
+def read_mnist(path):
+    with gzip.open(path,"rb") as f:
+        ((xtrain,ytrain),(xvalid,yvalid), _) = pickle.load(f,encoding="latin-1")
+    return xtrain,ytrain,xvalid,yvalid
+
+def map_to_tensor(xtrain,ytrain,xvalid,yvalid):
+    xtrain,ytrain,xvalid,yvalid = map(torch.tensor,(xtrain,ytrain,xvalid,yvalid))
+    return xtrain,ytrain,xvalid,yvalid
+
+if __name__ == "__main__":
+
+    data_path = Path("data") if Path("data").exists() else Path("./data")
+    path = data_path / "downloaded" / "vector-mnist"
+    path.mkdir(parents=True, exist_ok=True)
+
+    datafile = download_data(path)
+    print(datafile)
+    xtrain,ytrain,xvalid,yvalid = read_mnist(datafile)
+    print(xtrain.shape)
+
+    datalist = [xtrain,ytrain,xvalid,yvalid]
+    xtrain,ytrain,xvalid,yvalid = map_to_tensor(xtrain,ytrain,xvalid,yvalid)
+    print(xtrain.shape)
+    
+    idx = random.randint(0,len(xtrain))
+    example = xtrain[idx]
+
+    print(ytrain[idx])
+    wandb.Image(example.reshape(1,28,28)).image
+    
+
