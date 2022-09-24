84c109829aef7232d136b93c86500b82c0233b40
Shoaib Khan <shoaibkhanz@hotmail.com>
2022-09-24 15:48:21 +0100

    added black to toml

--- data_utils.py
+++ data_utils.py
@@ -1,8 +1,10 @@
+import gzip
+import math
+import pickle
 from pathlib import Path
+
 import requests
 import torch
-import pickle
-import gzip
 
 
 def download_data(path):
@@ -23,10 +25,45 @@ def download_data(path):
 
 def read_mnist(path):
     with gzip.open(path, "rb") as f:
-        ((xtrain, ytrain), (xvalid, yvalid), _) = pickle.load(f, encoding="latin-1")
+        ((xtrain, ytrain), (xvalid, yvalid), _) = pickle.load(
+            f, encoding="latin-1"
+        )
     return xtrain, ytrain, xvalid, yvalid
+
 
 def map_to_tensor(xtrain, ytrain, xvalid, yvalid):
-    xtrain, ytrain, xvalid, yvalid = map(torch.tensor, (xtrain, ytrain, xvalid, yvalid))
+    xtrain, ytrain, xvalid, yvalid = map(
+        torch.tensor, (xtrain, ytrain, xvalid, yvalid)
+    )
     return xtrain, ytrain, xvalid, yvalid
 
+
+def linear(x, weights, bias):
+    return x @ weights + bias
+
+
+def log_softmax(x: torch.Tensor) -> torch.Tensor:
+    return x - torch.log(torch.sum(torch.exp(x), axis=1))[:, None]
+
+
+def cross_entropy(pred, actual):
+    return -pred[range(actual.shape[0]), actual].mean()
+
+
+def mnist_wb(nclasses: int = 10) -> torch.Tensor:
+    weights = torch.randn(784, nclasses) / math.sqrt(784)
+    weights.requires_grad_()
+    bias = torch.zeros(nclasses, requires_grad=True)
+    return weights, bias
+
+
+def prepare_data() -> torch.Tensor:
+    data_path = (
+        Path("data") if Path("data").exists() else Path("./data")
+    )
+    path = data_path / "downloaded" / "vector-mnist"
+    path.mkdir(parents=True, exist_ok=True)
+
+    datafile = download_data(path)
+    xtrain, ytrain, xvalid, yvalid = read_mnist(datafile)
+    return xtrain, ytrain, xvalid, yvalid
--- main.py
+++ main.py
@@ -1,35 +1,40 @@
-import math
 import random
 from pathlib import Path
 
 import torch
 import wandb
 
-from data_utils import download_data, map_to_tensor, read_mnist
+from data_utils import (
+    cross_entropy,
+    linear,
+    log_softmax,
+    map_to_tensor,
+    mnist_wb,
+    prepare_data,
+)
 
-weights = torch.randn(784, 10) / math.sqrt(784)
-weights.requires_grad_()
-bias = torch.zeros(10, requires_grad=True)
-print(weights.shape)
-print(bias.shape)
+if __name__ == "__main__":
 
-data_path = Path("data") if Path("data").exists() else Path("./data")
-path = data_path / "downloaded" / "vector-mnist"
-path.mkdir(parents=True, exist_ok=True)
+    xtrain, ytrain, xvalid, yvalid = prepare_data()
 
-datafile = download_data(path)
-print(datafile)
-xtrain, ytrain, xvalid, yvalid = read_mnist(datafile)
-print(xtrain.shape)
+    xtrain, ytrain, xvalid, yvalid = map_to_tensor(
+        xtrain, ytrain, xvalid, yvalid
+    )
+    print(xtrain.shape)
 
-datalist = [xtrain, ytrain, xvalid, yvalid]
-xtrain, ytrain, xvalid, yvalid = map_to_tensor(xtrain, ytrain, xvalid, yvalid)
-print(xtrain.shape)
+    idx = random.randint(0, len(xtrain))
+    example = xtrain[idx]
 
-idx = random.randint(0, len(xtrain))
-example = xtrain[idx]
+    print(ytrain[idx])
+    wandb.Image(example.reshape(1, 28, 28)).image
 
-print(ytrain[idx])
-wandb.Image(example.reshape(1, 28, 28)).image
+    bs = 64
+    xb = xtrain[:bs]
+    yb = ytrain[:bs]
+    weights, bias = mnist_wb()
+    preds = log_softmax(linear(xb, weights=weights, bias=bias))
 
-# if __name__ == "__main__":
+    loss_func = cross_entropy
+    loss = loss_func(preds, yb)
+    print(f"loss: {loss}")
+    print(bias.grad)
